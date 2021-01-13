e16a858d2d81fa6bdbfd5d951e1eeab79869602f
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2021-01-13 21:53:09 +0100

    camarchepasbien

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_114859) do
+ActiveRecord::Schema.define(version: 2021_01_11_210732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "product_id"
+    t.integer "cart_id"
+    t.integer "order_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "sku"
+    t.string "name"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "price_cents", default: 0, null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +65,5 @@ ActiveRecord::Schema.define(version: 2021_01_10_114859) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "categories"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# PRODUCT
+puts 'Cleaning database...'
+Product.destroy_all
+Category.destroy_all
+
+puts 'Creating categories...'
+epilation = Category.create!(name: 'epilation')
+massage = Category.create!(name: 'massages')
+
+puts 'Creating products...'
+Product.create!(sku: 'massage thai', name: 'massage thai', category: massage) 
+Product.create!(sku: 'poil trop longs', name: 'polis trop longs', category: epilation)
+Product.create!(sku: 'poil des pieds',   name: 'poils des pieds',      category: epilation)
+Product.create!(sku: 'raoul',   name: 'le cri qui desaoule',      category: epilation)
+puts 'Finished!'
